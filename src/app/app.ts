84a0634c5989486e982bae2869dc67d337b852d3
Dineo76 <dineomakofane2@gmail.com>
2026-05-21 22:08:25 +0200

    added css

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Contacts } from './contacts/contacts';
 import { Lessons } from './lessons/lessons';
+import { Services } from './services/services';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Contacts, Lessons],
+  imports: [RouterOutlet, Header, Contacts, Lessons, Services],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
